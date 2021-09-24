8721e248bc0b0eca43b28c4ac43d9ed0e99c9691
ahasaad <ahasaad@cisco.com>
2021-09-24 18:09:31 +0200

    fix tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there again!
+          learn react
         </a>
       </header>
     </div>
