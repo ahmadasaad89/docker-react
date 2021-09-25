825bae14621429d84a29155434df700a47e39ba2
ahasaad <ahasaad@cisco.com>
2021-09-25 12:41:56 +0200

    change main branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react 1
         </a>
       </header>
     </div>
